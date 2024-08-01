9f78efef8a05b3c739d4192d15b4b6559cac8fa3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 07:56:48 +0530

    getAllJobs Function Keyword Search: Extracts the keyword query parameter from the request (defaulting to an empty string if not provided). Query Construction: Constructs a MongoDB query to search for jobs where the title or description contains the keyword (case-insensitive). Job Lookup: Finds all jobs matching the query, populates the company details, and sorts them by creation date in descending order. Response: Returns a 200 status code with the found jobs if jobs are found, otherwise returns a 404 status code with a "Jobs not found" message.

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -33,6 +33,32 @@ export const postJob = async (req, res) => {
         console.log(error);
     }
 }
-
+// student k liye
+export const getAllJobs = async (req, res) => {
+    try {
+        const keyword = req.query.keyword || "";
+        const query = {
+            $or: [
+                { title: { $regex: keyword, $options: "i" } },
+                { description: { $regex: keyword, $options: "i" } },
+            ]
+        };
+        const jobs = await Job.find(query).populate({
+            path: "company"
+        }).sort({ createdAt: -1 });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
