6e088939a8404cf99153d4a70cc346d52b82bfe9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:16:23 +0530

    Import Statements Importing Modules: express: Imports the Express framework to create and manage routes and handle HTTP requests. isAuthenticated: Imports middleware from isAuthenticated.js that checks if a user is authenticated before allowing access to certain routes. getAdminJobs, getAllJobs, getJobById, postJob: Imports controller functions from job.controller.js which handle the business logic for various job-related operations.

---
+++ backend/routes/job.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
+
+
+
+
+
+
+
