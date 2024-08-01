14298d646089123b6d10772371230768013884b3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:28:54 +0530

    Imports React, { useState }: Imports React and the useState hook for managing local state. Button: Imports a custom button component. Search: Imports an icon from the lucide-react library. useDispatch: Imports the Redux hook used to dispatch actions. setSearchedQuery: Imports an action creator from the jobSlice to update the search query in the Redux store. useNavigate: Imports the hook for programmatic navigation.

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { useDispatch } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import { useNavigate } from 'react-router-dom';
+
+
+
