c6cb134dd27219859407ecd995c8b4e0899cf844
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:20:43 +0530

    Middleware Configuration app.use(express.json());: Middleware to parse JSON bodies in incoming requests. app.use(express.urlencoded({ extended: true }));: Middleware to parse URL-encoded bodies in incoming requests (such as form submissions). extended: true allows for richer data parsing. app.use(cookieParser());: Middleware to parse cookies attached to the client request object. CORS Configuration: const corsOptions = { origin: 'http://localhost:5173', credentials: true };: Defines options for CORS. origin: Specifies the allowed origin(s) for cross-origin requests. Here, only requests from http://localhost:5173 are allowed. credentials: Allows cookies and credentials to be sent in cross-origin requests. app.use(cors(corsOptions));: Applies CORS middleware with the specified options. Port Configuration const PORT = process.env.PORT || 3000;: Defines the port number for the server to listen on. It uses the port specified in the environment variable PORT, or defaults to 3000 if PORT is not defined.

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,16 @@ dotenv.config({});
 
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
 
+app.use(cors(corsOptions));
 
 
 
