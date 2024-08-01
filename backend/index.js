1186625c3170f261734250c2161acf23aabf5f6d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:20:20 +0530

    Import Statements express: Imports the Express framework for building the server. cookieParser: Imports middleware for parsing cookies from HTTP requests. cors: Imports middleware to enable Cross-Origin Resource Sharing (CORS), which allows requests from different origins. dotenv: Imports the dotenv package to manage environment variables from a .env file. connectDB: Imports a utility function for connecting to the MongoDB database. userRoute, companyRoute, jobRoute, applicationRoute: Imports route handlers for different parts of the application (user management, company management, job management, and application management).

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
