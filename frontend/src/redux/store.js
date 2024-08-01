a1970e0c7892801236a659943b2806b34fffc380
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:46:42 +0530

    Persistence Configuration persistConfig key: 'root': Specifies the key under which the persisted state is stored in the storage engine. version: 1: Indicates the version of the persisted state schema. This helps in managing migrations between different versions of the state schema. storage: Refers to the storage engine used to persist the state (local storage in this case).

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -15,7 +15,11 @@ import storage from 'redux-persist/lib/storage'
 import companySlice from "./companySlice";
 import applicationSlice from "./applicationSlice";
 
-
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
 
 
 
