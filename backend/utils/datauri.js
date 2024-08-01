7d99770e80cab9661b0686457b0b5285fc30300f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:14:37 +0530

    Data URI Conversion Creating a Parser Instance: const parser = new DataUriParser(); creates an instance of the DataUriParser class. This instance will be used to generate the Data URI.
    Getting File Extension: const extName = path.extname(file.originalname).toString(); extracts the file extension from the originalname property of the file object.
    
    path.extname(): This method gets the file extension from the file's original name (e.g., .jpg, .png, .pdf).
    .toString(): Ensures the file extension is a string.
    Generating Data URI: return parser.format(extName, file.buffer); formats the file as a Data URI using the DataUriParser instance.
    
    file.buffer: The buffer containing the file’s binary data.
    parser.format(): Converts the file buffer and its MIME type (derived from the file extension) into a Data URI.

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -3,6 +3,8 @@ import DataUriParser from "datauri/parser.js"
 import path from "path";
 
 const getDataUri = (file) => {
-    
+    const parser = new DataUriParser();
+    const extName = path.extname(file.originalname).toString();
+    return parser.format(extName, file.buffer);
 }
 
