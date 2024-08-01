6db2c114a45a5bda45f90c00cc7ddb45221c9ffc
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:32:37 +0530

    State and Data Access const { allJobs } = useSelector(store => store.job); Uses the useSelector hook to access the allJobs array from the Redux store’s job slice. allJobs contains the list of job objects.

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 // const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const LatestJobs = () => {
-    
+    const {allJobs} = useSelector(store=>store.job);
    
     
 }
