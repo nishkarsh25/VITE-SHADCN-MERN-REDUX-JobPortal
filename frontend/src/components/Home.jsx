b0b2b10f233d5267179839973e80ce972aa2db0f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:27:38 +0530

    useEffect Hook useEffect(() => { if (user?.role === 'recruiter') { navigate("/admin/companies"); } }, []); Executes a side effect when the component mounts: Checks if the logged-in user’s role is 'recruiter'. If true, navigates to the /admin/companies route. The empty dependency array [] ensures that this effect runs only once when the component mounts.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,7 +12,11 @@ const Home = () => {
   useGetAllJobs();
   const { user } = useSelector(store => store.auth);
   const navigate = useNavigate();
-  
+  useEffect(() => {
+    if (user?.role === 'recruiter') {
+      navigate("/admin/companies");
+    }
+  }, []);
   return
 }
 
