36ac69403fbd6d9b8e9120ebc71c9b7be45e4197
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 07:59:35 +0530

    getApplicants Function Job ID: Gets the job ID from the request parameters. Finding Job and Applicants: Finds the job by ID and populates the applications array with details of each applicant, sorted by creation date in descending order. Response: Returns a 200 status code with the job and its applicants if the job is found, otherwise returns a 404 status code indicating the job does not exist.

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -70,4 +70,28 @@ export const getAppliedJobs = async (req,res) => {
         console.log(error);
     }
 }
-
+// admin dekhega kitna user ne apply kiya hai
+export const getApplicants = async (req,res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant'
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message:'Job not found.',
+                success:false
+            })
+        };
+        return res.status(200).json({
+            job, 
+            succees:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
