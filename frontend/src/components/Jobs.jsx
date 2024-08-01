329f92c183c3aac0fbaf4c8c77430856c195f8ed
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:36:32 +0530

    Imports React, { useEffect, useState }: Imports React along with hooks for state management and side effects. Navbar: Imports a Navbar component for navigation. FilterCard: Imports a FilterCard component for filtering job listings. Job: Imports a Job component to render individual job postings. useSelector: Imports the useSelector hook from react-redux to access the Redux store state. motion: Imports motion from framer-motion for animation effects.

---
+++ frontend/src/components/Jobs.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './shared/Navbar'
+import FilterCard from './FilterCard'
+import Job from './Job';
+import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
+
+// const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+
+
