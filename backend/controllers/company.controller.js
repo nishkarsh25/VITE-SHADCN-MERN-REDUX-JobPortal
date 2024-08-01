6ed984739d95d5e5f5f47895fb82bc1bd4e86271
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 07:57:58 +0530

    getCompany Function User ID: Gets the user ID from the request (assuming it's set by a middleware). Company Lookup: Finds all companies associated with the logged-in user ID. Response: Returns a 200 status code with the found companies if companies are found, otherwise returns a 404 status code with a "Companies not found" message.

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -32,5 +32,22 @@ export const registerCompany = async (req, res) => {
         console.log(error);
     }
 }
-
+export const getCompany = async (req, res) => {
+    try {
+        const userId = req.id; // logged in user id
+        const companies = await Company.find({ userId });
+        if (!companies) {
+            return res.status(404).json({
+                message: "Companies not found.",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            companies,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
