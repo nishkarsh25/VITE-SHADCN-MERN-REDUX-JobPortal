6b09c7441d3cc1b16b6cecda79c2b505c0872720
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:04:12 +0530

    Import Statements import React from 'react'
    Imports React to use JSX and create the component.
    import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
    
    Imports components for creating a popover UI element from a local popover file. These are used to create a dropdown menu for the user profile.
    import { Button } from '../ui/button'
    
    Imports a Button component from a local button file. This is used for various button elements in the navbar.
    import { Avatar, AvatarImage } from '../ui/avatar'
    
    Imports Avatar and AvatarImage components for displaying user profile pictures from a local avatar file.
    import { LogOut, User2 } from 'lucide-react'
    
    Imports icons (LogOut and User2) from the lucide-react library for logout and user profile actions.
    import { Link, useNavigate } from 'react-router-dom'
    
    Imports Link for navigation and useNavigate for programmatic navigation from the react-router-dom library.
    import { useDispatch, useSelector } from 'react-redux'
    
    Imports hooks from react-redux to interact with the Redux store. useDispatch is used to dispatch actions, and useSelector is used to access the Redux state.
    import axios from 'axios'
    
    Imports the axios library for making HTTP requests.
    import { USER_API_END_POINT } from '@/utils/constant'
    
    Imports the constant USER_API_END_POINT which contains the base URL for the user-related API endpoints.
    import { setUser } from '@/redux/authSlice'
    
    Imports the setUser action creator from authSlice in the Redux store. This action is used to update the user state in the Redux store.
    import { toast } from 'sonner'
    
    Imports the toast object from the sonner library to display notifications to the user.

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { LogOut, User2 } from 'lucide-react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { setUser } from '@/redux/authSlice'
+import { toast } from 'sonner'
+
+
+
