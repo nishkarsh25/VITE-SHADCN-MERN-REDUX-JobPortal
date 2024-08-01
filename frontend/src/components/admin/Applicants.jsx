3d2f48f4e64a6c294c85ea1a56325b6c91472d53
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:32:59 +0530

    Purpose The component:
    Fetches a list of applicants for a specific job.
    Displays this list in a table format.
    Provides a user interface with navigation and data display.
    Component Structure
    Imports:
    
    React:
    useEffect: Hook to perform side effects in the component, like fetching data from an API.
    Custom Components:
    Navbar: A navigation bar component for the top of the page.
    ApplicantsTable: Component that displays the list of applicants in a table format.
    Axios:
    axios: Library for making HTTP requests.
    Constants:
    APPLICATION_API_END_POINT: The endpoint URL for application-related API requests.
    React Router:
    useParams: Hook to access URL parameters.
    Redux:
    useDispatch: Hook to dispatch actions to the Redux store.
    useSelector: Hook to access the Redux store’s state.
    setAllApplicants: Action to set the list of applicants in the Redux store.

---
+++ frontend/src/components/admin/Applicants.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react'
+import Navbar from '../shared/Navbar'
+import ApplicantsTable from './ApplicantsTable'
+import axios from 'axios';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllApplicants } from '@/redux/applicationSlice';
+
+
+
