bca12025a9e9c7990664521a557fd541baad1e7c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:47:23 +0530

    Store Configuration store configureStore: Creates a Redux store with the persistedReducer as the reducer. This store is capable of persisting state using redux-persist. middleware: The getDefaultMiddleware function is used to set up the default middleware for the Redux store. The serializableCheck option is configured to ignore actions related to persistence (FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER) which might otherwise cause issues with serialization.

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -31,4 +31,12 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
-
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
