64e95a5a002e24efad8d45078fca43f4055f9823
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:56:33 +0530

    Import Statements import { setCompanies } from '@/redux/companySlice'
    Imports the setCompanies action creator from the companySlice file in the Redux store. This action is used to update the Redux state with the list of companies.
    import { COMPANY_API_END_POINT } from '@/utils/constant'
    
    Imports the constant COMPANY_API_END_POINT from the constant file. This constant holds the base URL for the company-related API endpoints.
    import axios from 'axios'
    
    Imports the axios library, which is used to make HTTP requests.
    import { useEffect } from 'react'
    
    Imports the useEffect hook from React. This hook is used to handle side effects, such as data fetching, in functional components.
    import { useDispatch } from 'react-redux'
    
    Imports the useDispatch hook from react-redux. This hook provides a way to dispatch actions to the Redux store.

---
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -0,0 +1,8 @@
+import { setCompanies} from '@/redux/companySlice'
+import { COMPANY_API_END_POINT} from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+
+
