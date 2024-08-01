a6a9cc7afa24fe15e1be62402c22ae363930f60b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:36:10 +0530

    Accessing State:
    user: Fetched from the Redux store using useSelector. This state contains information about the currently authenticated user, including their role.
    Navigation:
    
    navigate: Provides a function to change the URL programmatically. Used to redirect the user if they do not meet the criteria.

--- frontend/src/components/admin/ProtectedRoute.jsx
+++ frontend/src/components/admin/ProtectedRoute.jsx
@@ -3,7 +3,10 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({children}) => {
-   
+    const {user} = useSelector(store=>store.auth);
+
+    const navigate = useNavigate();
+
     
 
     return 
