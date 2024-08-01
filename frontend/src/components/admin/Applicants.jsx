87008d57dbadf6513c1119547480620135cf7d43
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:33:29 +0530

    Rendering:
    Navbar: Included at the top of the page.
    Container:
    Title: Displays a heading with the number of applicants.
    ApplicantsTable: Renders the table of applicants, assuming this component will fetch data from the Redux store and display it.

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -23,6 +23,14 @@ const Applicants = () => {
         }
         fetchAllApplicants();
     }, []);
-    return
+    return (
+        <div>
+            <Navbar />
+            <div className='max-w-7xl mx-auto'>
+                <h1 className='font-bold text-xl my-5'>Applicants {applicants?.applications?.length}</h1>
+                <ApplicantsTable />
+            </div>
+        </div>
+    )
 }
 
