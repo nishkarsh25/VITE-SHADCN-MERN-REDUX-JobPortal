dd9cfd6086e71d770c979fd71e9911e7ee77cdd8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 07:58:06 +0530

    getCompanyById Function Company ID: Gets the company ID from the request parameters. Company Lookup: Finds the company by ID. Response: Returns a 200 status code with the company details if the company is found, otherwise returns a 404 status code with a "Company not found" message.

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -50,4 +50,22 @@ export const getCompany = async (req, res) => {
         console.log(error);
     }
 }
-
+// get company by id
+export const getCompanyById = async (req, res) => {
+    try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if (!company) {
+            return res.status(404).json({
+                message: "Company not found.",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            company,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
