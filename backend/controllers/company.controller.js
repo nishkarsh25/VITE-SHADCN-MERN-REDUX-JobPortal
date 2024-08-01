75830e57a196ae6a35e776fc51045b4b4e58fb04
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 07:57:49 +0530

    registerCompany Function Destructuring Request Body: Extracts companyName from the request body. Input Validation: Checks if the companyName is provided, returning a 400 status code if it's missing. Company Existence Check: Checks if a company with the provided name already exists in the database. If yes, returns a 400 status code. Company Creation: Creates a new company document in MongoDB with the provided company name and the ID of the logged-in user (req.id). Response: Returns a 201 status code with a success message and the created company details if the company is registered successfully.

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -2,6 +2,35 @@ import { Company } from "../models/company.model.js";
 import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
 
+export const registerCompany = async (req, res) => {
+    try {
+        const { companyName } = req.body;
+        if (!companyName) {
+            return res.status(400).json({
+                message: "Company name is required.",
+                success: false
+            });
+        }
+        let company = await Company.findOne({ name: companyName });
+        if (company) {
+            return res.status(400).json({
+                message: "You can't register same company.",
+                success: false
+            })
+        };
+        company = await Company.create({
+            name: companyName,
+            userId: req.id
+        });
 
+        return res.status(201).json({
+            message: "Company registered successfully.",
+            company,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
