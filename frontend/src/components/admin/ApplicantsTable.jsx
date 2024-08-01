34eb61eba7a75d5bf560ac8ed239e6d7057af7be
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:34:50 +0530

    Functions:
    statusHandler:
    Parameters: status (the new status to set) and id (the ID of the applicant whose status is being updated).
    Purpose: Sends an HTTP POST request to update the status of a specific applicant. It uses Axios to send the request to the API endpoint.
    Error Handling: Displays an error message using toast if the request fails.
    Success Handling: Displays a success message using toast if the request is successful.

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -12,7 +12,19 @@ const shortlistingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
     const { applicants } = useSelector(store => store.application);
 
-    
+    const statusHandler = async (status, id) => {
+        console.log('called');
+        try {
+            axios.defaults.withCredentials = true;
+            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, { status });
+            console.log(res);
+            if (res.data.success) {
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
 
     return 
 }
