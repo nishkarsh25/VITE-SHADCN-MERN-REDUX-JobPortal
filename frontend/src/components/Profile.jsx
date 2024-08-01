5519fdf497e3eecc21397709c2079b2be7d5d62c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:08:23 +0530

    Imports React and Hooks: useState for managing component state. UI Components: Navbar: A navigation bar component. Avatar, AvatarImage: For displaying the user's profile picture. Button: For interactive buttons. Contact, Mail, Pen: Icons from lucide-react. Badge: For displaying skills and other information in a badge format. Label: For labeling sections of the profile. AppliedJobTable: Component to display a table of jobs the user has applied for. UpdateProfileDialog: A dialog component for updating profile details. Redux: useSelector: Hook to access Redux store state. Custom Hook: useGetAppliedJobs: Custom hook to fetch the list of jobs the user has applied for.

---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Navbar from './shared/Navbar'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Button } from './ui/button'
+import { Contact, Mail, Pen } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { Label } from './ui/label'
+import AppliedJobTable from './AppliedJobTable'
+import UpdateProfileDialog from './UpdateProfileDialog'
+import { useSelector } from 'react-redux'
+import useGetAppliedJobs from '@/hooks/useGetAppliedJobs'
+
+// const skills = ["Html", "Css", "Javascript", "Reactjs"]
+const isResume = true;
+
+
+
