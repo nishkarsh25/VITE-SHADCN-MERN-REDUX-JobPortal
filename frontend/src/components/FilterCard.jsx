1546af3d4537d38b1f108500d58adfdec2f09649
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:06:18 +0530

    Detailed Breakdown Imports
    React: Essential for creating and managing the component.
    RadioGroup, RadioGroupItem: Custom UI components for creating radio buttons, used to allow users to select a filter option.
    Label: A component to display labels associated with radio buttons.
    useDispatch: A Redux hook to dispatch actions to the Redux store.
    setSearchedQuery: An action creator from the Redux store to set the current search query.

---
+++ frontend/src/components/FilterCard.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
+import { useDispatch } from 'react-redux'
+import { setSearchedQuery } from '@/redux/jobSlice'
+
+
+
+
