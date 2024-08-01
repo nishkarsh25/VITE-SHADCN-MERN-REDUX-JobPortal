ed3e4fdb1621c44a9e4fd94650f2c852615da0ff
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:17:38 +0530

    Router Setup Creating a Router Instance: const router = express.Router(); initializes an Express Router instance. This router is used to define routes and handle requests related to companies.

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -3,7 +3,7 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
 import { singleUpload } from "../middlewares/mutler.js";
 
-
+const router = express.Router();
 
 
 
