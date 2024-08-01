6cf76a0e7502e4ac831ebc2566f0ef2e4859c62a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:21:04 +0530

    Effect Hook:
    useEffect:
    It runs whenever the companies list or searchCompanyByText changes.
    Filters the companies list based on the search query (searchCompanyByText).
    If searchCompanyByText is empty, all companies are shown.
    Otherwise, it filters companies whose names include the search query (case-insensitive).

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -10,7 +10,16 @@ const CompaniesTable = () => {
     const { companies, searchCompanyByText } = useSelector(store => store.company);
     const [filterCompany, setFilterCompany] = useState(companies);
     const navigate = useNavigate();
-    
+    useEffect(()=>{
+        const filteredCompany = companies.length >= 0 && companies.filter((company)=>{
+            if(!searchCompanyByText){
+                return true
+            };
+            return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+
+        });
+        setFilterCompany(filteredCompany);
+    },[companies,searchCompanyByText])
     return 
 }
 
