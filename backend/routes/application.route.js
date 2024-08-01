6c9d25119ac92c740fb792fd4f32651dcba4526e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:19:24 +0530

    Router Setup Creating a Router Instance: const router = express.Router(); initializes an Express Router instance. This router is used to define routes and handle requests related to job applications.

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
  
-
+const router = express.Router();
 
 
  
