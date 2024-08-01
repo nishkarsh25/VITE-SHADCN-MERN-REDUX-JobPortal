b8fa1d54cb3f5dffdaca15336cfdcb4e1d33ce0f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:58:31 +0530

    Import Statements import { setAllJobs } from '@/redux/jobSlice'
    Imports the setAllJobs action creator from the jobSlice file in the Redux store. This action is used to update the Redux state with the list of jobs.
    import { JOB_API_END_POINT } from '@/utils/constant'
    
    Imports the constant JOB_API_END_POINT from the constant file. This constant holds the base URL for the job-related API endpoints.
    import axios from 'axios'
    
    Imports the axios library, which is used to make HTTP requests.
    import { useEffect } from 'react'
    
    Imports the useEffect hook from React. This hook is used to handle side effects, such as data fetching, in functional components.
    import { useDispatch, useSelector } from 'react-redux'
    
    Imports the useDispatch and useSelector hooks from react-redux. useDispatch is used to dispatch actions to the Redux store, and useSelector is used to access the Redux store's state.

---
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -0,0 +1,8 @@
+import { setAllJobs } from '@/redux/jobSlice'
+import { JOB_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+
+
