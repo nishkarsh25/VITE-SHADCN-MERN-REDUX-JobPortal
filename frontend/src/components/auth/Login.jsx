b07aa452a0d21a098f1f1a0579bd3a2447324166
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:07:54 +0530

    Imports React, { useEffect, useState }: Imports React and hooks for managing state and side effects. Navbar: Imports a navigation bar component to display at the top of the page. Label, Input, RadioGroup, Button: Imports UI components for form elements and buttons. Link, useNavigate: Imports routing components for navigation. axios: Imports Axios for making HTTP requests. USER_API_END_POINT: Imports the API endpoint for user-related operations. toast: Imports a notification library for displaying messages. useDispatch, useSelector: Imports Redux hooks for dispatching actions and accessing state. setLoading, setUser: Imports Redux actions for managing loading state and user information. Loader2: Imports an icon component for showing a loading spinner.

---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { RadioGroup } from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { toast } from 'sonner'
+import { useDispatch, useSelector } from 'react-redux'
+import { setLoading, setUser } from '@/redux/authSlice'
+import { Loader2 } from 'lucide-react'
+
+
+
