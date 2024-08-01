f8de58bcccb197ae4dd7d70e05bde75bc4e90ed3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:13:10 +0530

    Dependencies and Imports React: Essential for building the component. UI Components: Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow: These components are used to construct and style the table. Badge: A component used to display the application status with different styles. Redux: useSelector: A hook from React-Redux to access the Redux store's state, specifically to retrieve the list of applied jobs.

---
+++ frontend/src/components/AppliedJobTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import { Badge } from './ui/badge'
+import { useSelector } from 'react-redux'
+
+
+
