c8c1c7eea6f20c48043426b2f8c07e19d15aeb5d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:17:28 +0530

    Import Statements express: Imports the Express framework to create and manage routes and handle HTTP requests. isAuthenticated: Imports middleware from isAuthenticated.js which ensures that a user is authenticated before accessing protected routes. getCompany, getCompanyById, registerCompany, updateCompany: Imports controller functions from company.controller.js that handle the business logic for company-related operations. singleUpload: Imports middleware from multer.js for handling file uploads.

---
+++ backend/routes/company.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/mutler.js";
+
+
+
+
+
