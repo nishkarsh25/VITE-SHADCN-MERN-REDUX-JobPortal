a1c613c991b27cbdb4e63f46d1ecf816d579a358
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 08:18:08 +0530

    Export Statement Exporting the Router: export default router; exports the configured router instance as the default export from the module. Usage: This allows the router to be imported into the main application file (app.js or server.js) and integrated into the Express application, thereby setting up the routes for handling company-related requests. Summary Purpose: The code configures routes for company-related operations such as registering a company, retrieving company details, and updating company information. Middleware Usage: Utilizes isAuthenticated to protect routes and ensure that only authenticated users can access them. singleUpload middleware handles file uploads for updating company-related files. Router Configuration: Defines routes and associates them with HTTP methods and controller functions for managing companies. Export: The router is exported for integration into the main Express application.

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -10,3 +10,5 @@ router.route("/get").get(isAuthenticated,getCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
 router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
 
+export default router;
+
