d9d62a95dbdfe12ee68b5da9a623c5bb926b5443
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 10:14:57 +0530

    Component Purpose The Browse component is used to render a list of jobs, with each job displayed using the Job component. It fetches all available jobs, displays them in a grid layout, and clears the search query when the component unmounts.
    Dependencies and Imports
    React: Core library for building the component.
    Navbar: A shared component used for navigation.
    Job: A component that represents an individual job in the list.
    Redux:
    useDispatch: A hook used to dispatch actions to the Redux store.
    useSelector: A hook used to access the Redux store’s state.
    Custom Hooks:
    useGetAllJobs: A custom hook that fetches all jobs and updates the Redux store.
    Redux Actions:
    setSearchedQuery: An action dispatched to clear the search query from the Redux store.

---
+++ frontend/src/components/Browse.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+import Navbar from './shared/Navbar'
+import Job from './Job';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import useGetAllJobs from '@/hooks/useGetAllJobs';
+
+// const randomJobs = [1, 2,45];
+
+
