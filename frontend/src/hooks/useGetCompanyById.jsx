834c5af694357eaf94ddb55ccffca03cc61f6e39
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-01 09:00:57 +0530

    Import Statements import { setSingleCompany } from '@/redux/companySlice'
    Imports the setSingleCompany action creator from the companySlice file in the Redux store. This action will be used to update the Redux state with the details of a single company.
    import { setAllJobs } from '@/redux/jobSlice'
    
    Although setAllJobs is imported, it is not used in this particular hook. It may be part of the codebase for future use or inclusion.
    import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
    
    Imports the constants COMPANY_API_END_POINT and JOB_API_END_POINT from the constant file. These constants contain the base URLs for the API endpoints related to companies and jobs, respectively.
    import axios from 'axios'
    
    Imports the axios library, which is used for making HTTP requests.
    import { useEffect } from 'react'
    
    Imports the useEffect hook from React. This hook is used to handle side effects in functional components, such as data fetching.
    import { useDispatch } from 'react-redux'
    
    Imports the useDispatch hook from react-redux. This hook provides access to the dispatch function, allowing you to dispatch actions to the Redux store.

---
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -0,0 +1,9 @@
+import { setSingleCompany } from '@/redux/companySlice'
+import { setAllJobs } from '@/redux/jobSlice'
+import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+
+
